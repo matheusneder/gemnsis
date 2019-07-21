804e02c054f092a7bd463eeeca07634190bcbe2f
Matheus Neder <matheusneder@gmail.com>
2019-07-21 19:27:16 -0300

    Added some profile validation and tests

--- nukr-logic/src/nukr_logic/core.clj
+++ nukr-logic/src/nukr_logic/core.clj
@@ -7,13 +7,76 @@
 
 (def duplicated-profile-key-error "duplicated-profile-key-error")
 
-(defn add-profile 
+(def profile-validation-errors
+  {:duplicated-key
+   {:code "profile-duplicated-key"
+    :message "Profile key must be unique."}
+   :blank-name
+   {:code "profile-blank-name"
+    :message "The field name must not be blank."}
+   :invalid-name
+   {:code "invalid-profile-name"
+    :message "Profile name must contais only letters 
+and it must has at least 3 letters and max of 50 letters."}
+   :duplicated-name
+   {:code "profile-duplicated-name"
+    :message "The profile name has already been 
+taken for another person, please, choose another."}})
+
+(defn add-error-reason [data error-reason]
+  (assoc
+   data
+   :errors (conj (-> data :errors)
+                 error-reason)))
+
+(defn validate-profile-email [profile-data]
+  profile-data)
+
+(defn validate-profile-name [profile-data]
+  "Validate if profile name is not blank then if has only letters
+and its size is between 3 and 30."
+  (if
+   (clojure.string/blank? (-> profile-data :name))
+    ;; name is invalid: it is blank
+    (add-error-reason
+     profile-data
+     (-> profile-validation-errors :blank-name))
+    ;; look for regex validation only if name is not blank
+    ;; in order to avoid to add two errors for the same 
+    ;; field (name)
+    (if
+      ;; Accept only letters and white space with
+      ;; size between 3 and 50 characters.
+      (re-matches #"^[A-Za-z ]{3,50}$" (-> profile-data :name))
+      ;; is a valid name, so will not touch the profile-data
+      profile-data
+      ;; name is invalid: add reason to error list
+      (add-error-reason
+       profile-data
+       (-> profile-validation-errors :invalid-name)))))
+
+(defn validate-profile [profile-data]
+  (-> profile-data
+      validate-profile-name
+      validate-profile-email))
+
+(defn add-profile
   [profile-map key data]
-  (if (contains? profile-map key)
-    [nil 'duplicated-profile-key-error]
-    (assoc profile-map key 
-           (assoc data
-                  :connections []))))
+  (let [verified-data (validate-profile data)]
+        (if (contains? verified-data :errors)
+          ;; profile data is NOT valid
+          ;; so return errors
+          {:errors (-> verified-data :errors)}
+          ;; profile data is valid
+          (if (contains? profile-map key)
+            ;; key already exists
+            ;; return error
+            {:errors '((-> profile-validation-errors :duplicated-key))}
+            ;; sounds good, adding the new profile with empty 
+            ;; connections vector
+            (assoc profile-map key 
+                   (assoc data
+                          :connections []))))))
  
  (defn add-to-profile-connection-id-vector
    [target-profile source-profile-id]
--- nukr-logic/test/nukr_logic/core_test.clj
+++ nukr-logic/test/nukr_logic/core_test.clj
@@ -13,23 +13,70 @@
           result (add-profile {}
                               new-profile-key
                               new-profile-data)]
-      (is (= new-profile-key
-                 (-> result keys last))
-              "profile key not match")
-      (is (= (assoc new-profile-data :connections [])
-                 (-> result vals last))
-              "profile data not match"))))
+      (is (= 
+           ;; expected
+           new-profile-key
+           ;; actual
+           (-> result keys last))
+          ;; assertion failure message
+          "profile key not match")
+      (is (= 
+           ;; expected
+           (assoc new-profile-data :connections [])
+           ;; actual
+           (-> result vals last))
+          ;; assertion failure message
+          "profile data not match"))))
 
 (deftest add-profile-duplicated-key-error-test
   (testing "add-profile-duplicated-key-error-test"
     (is (=
-         [nil 'duplicated-profile-key-error]
+         ;; expected
+         {:errors '((-> profile-validation-errors :duplicated-key))}
+         ;; actual
          (add-profile
-          {"the-duplicated-key" 
+          ;; current profile-map
+          {"the-duplicated-key"
            {:name "Foo" :visible true}}
-          "the-duplicated-key" 
+          ;; new item key
+          "the-duplicated-key"
+          ;; new item data
           {:name "Bar" :visible false}))
+        ;; assertion failure message
         "didnt get failed on duplicated profile key")))
+
+(deftest validate-profile-name-test
+  (testing "validate-profile-name"
+    (is (=
+         ;; expected
+         {:name "A Valid Name"}
+         ;; actual
+         (validate-profile-name {:name "A Valid Name"}))
+        ;; assertion failure message
+        "validation for 'A Valid Name' failed.")))
+
+(deftest validate-profile-name-error-blank
+  (testing "validate-profile-name-error-blank"
+    (is (some 
+         ;; expected (as an item of the errors list)
+         #(= (-> profile-validation-errors :blank-name) %)
+         ;; actual
+         (-> (validate-profile-name {:name "  "}) :errors))
+        ;; assertion failure message
+        "Provided a blank name but blank-name error reason could
+not be found.")
+    ))
+
+(deftest validate-profile-name-error-invalid
+  (testing "validate-profile-name-error-invalid"
+    (is (some
+         ;; expected (as an item of the errors list)
+         #(= (-> profile-validation-errors :invalid-name) %)
+         ;; actual
+         (-> (validate-profile-name {:name "Ab"}) :errors))
+        ;; assertion failure message
+        "Provided a invalid name but invalid-name error reason could
+not be found.")))
 
 (def profile-map-example
   {"abc" {:name "Matheus"
